c462e2c0028f70ee6c7dba129f64a8bd431caa4b
donconn3 <don.connelly@outlook.com>
2023-04-30 17:06:04 -0400

    Weather Search
    added search functionality to weather display. Placeholder text exists initially but then the search replaces the data

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-const key = 'TkCb2NZH6BoNFd4JYAOrm8EewMaCsEyhmfGksWhO';
+const nasaKey = 'TkCb2NZH6BoNFd4JYAOrm8EewMaCsEyhmfGksWhO';
 
 const getPictures = () =>{
     axios
-    .get('https://api.nasa.gov/planetary/apod?api_key=' + key)
+    .get('https://api.nasa.gov/planetary/apod?api_key=' + nasaKey)
     .then(response =>{
         const link = response.data;
         console.log(link.url);
@@ -11,7 +11,95 @@ const getPictures = () =>{
     })
     .catch(error => console.error(error))
 }
-
-const getWeather =() =>{
+const stateCode = (state) =>{
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+    for(let i = 0; i < states.length; i++){
+        if(states[i][0] === state){
+            return states[i][1];
+        }
     
+}
+
+}
+const getWeather =async () =>{
+const location = document.getElementById('search-box').value;
+
+const options = {
+  method: 'GET',
+  url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+  params: {
+    q: location,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'fd5ccc20c7msh779816541adcddcp1496e2jsn450166834c50',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  }
+};
+
+let data = {};
+
+try {
+	const response = await axios.request(options);
+	data = response.data;
+    document.getElementById('city-name').innerHTML = data.location.name + ', ' + stateCode(data.location.region);
+    document.getElementById('current-img').src = data.current.condition.icon;
+    document.getElementById('current-temp').innerHTML = Math.floor(data.current.temp_f) + '°F';
+    document.getElementById('current-conditions').innerHTML = data.current.condition.text;
+    document.getElementById('humidity-amount').innerHTML = Math.floor(data.current.humidity);
+    document.getElementById('wind-amount').innerHTML = Math.floor(data.current.wind_mph) + 'mph';
+} catch (error) {
+	console.error(error);
+}
+
 }
